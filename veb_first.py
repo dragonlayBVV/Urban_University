05b9b65437ba875488360df36c16fa56d6af095c
DragonlayBV <vitalybulavenko@yandex.ru>
2024-06-19 19:55:33 +0300

    Изменил два файла

--- veb_first.py
+++ veb_first.py
@@ -1,16 +1,3 @@
-spisok = [3,5,6,3,8,True,'hjf']
-print(type(spisok))
-corteg = (5,6,8,9,2,4,6)
-print(type(corteg))
-slovar = {1:'name', 2:'surname'}
-print(type(slovar))
-mnogestvo = {2, 5, 4, 3, 6, 5, 2}
-print(type(mnogestvo))
-a = 5
-print(type(a))
-b = 1.5
-print(type(b))
-c = 'work'
-print(type(c))
-
-grades_0 = sum(grades[0])/len(grades[0])
+s = [1,3,6,8,90,'sl']
+a = s.remove(1)
+print(a)
