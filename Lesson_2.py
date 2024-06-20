6c22b1bd57212454b2c33f97e2dcbc428580e117
DragonlayBV <vitalybulavenko@yandex.ru>
2024-06-20 19:10:00 +0300

    Вебинар 14.06

--- Lesson_2.py
+++ Lesson_2.py
@@ -1,1 +1,9 @@
-print('Hello world!') # изменил
+li = [3,'ghfj0']
+print(li)
+di = {2:1988,3:1800,1:5643,4:1800}
+print(di)
+mnog = {4,1,1,4,4,5,2,8,7,7,7,32}
+print(mnog)
+cor = (3,5,1,7,8)
+print(cor)
+print('Hi!')
