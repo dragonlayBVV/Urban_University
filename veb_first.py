0138a30afe3f8d7b309194841392c267b10e61fc
DragonlayBV <vitalybulavenko@yandex.ru>
2024-06-21 18:48:52 +0300

    Вебинар 14.06

---
+++ veb_first.py
@@ -0,0 +1,23 @@
+# a = 'hgj' #str
+# a = a +'gh'  #concatination
+# a = a * 5    #ymnogenie
+#
+# n = 1  #int
+#
+# n = ((((n**4+2)/2-3)*8)%6)//2
+# num = 1.5 #float
+
+# li = [4,3,6,'tgh'] #list
+# print(li)
+# li.pop(3)
+# print(li)
+# print(4 in li)
+# print(li[2])
+# di = {1:'Igor', 2:'Daniil'} #dict
+a = 5
+cor = ([6,7,8,9], {2,2,5,67,8},4,'5', {1:'Igor', 2:'Daniil'},7)  #tuple
+cor.pop()
+print(cor)
+
+
+# mnog = {3,4,2,3,3,2,2,7,8}  #set
